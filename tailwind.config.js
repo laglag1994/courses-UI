e5444971f564cc2d895b8a0a54c869e7ace48025
TajRask <roqayah1994@gmail.com>
2023-01-08 23:50:32 +0300

    17

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ module.exports = {
       lg:'976px',
       xl: '1440px'
     },
-    extend: {},
+    extend: {
+
+      colors:{
+        brightRed:'#EEB0A7',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightResSupLight:'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        veryPaleRed: 'hsl(12, 100%, 96%)',
+        veryLightGray: 'hsl(0,0%,98%)'
+        
+      },
+    },
   },
   plugins: [],
 }
