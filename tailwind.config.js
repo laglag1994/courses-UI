a3e0b22b2072882a76f20a87e02aa75ef3046d67
TajRask <roqayah1994@gmail.com>
2023-01-09 02:01:35 +0300

    border

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,9 @@ module.exports = {
     },
     extend: {
 
-      colors:{
-        brightRed:'#EEB0A7',
-        brightRedLight: 'hsl(12, 88%, 69%)',
-        brightResSupLight:'hsl(12, 88%, 95%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(223, 12%, 13%)',
-        veryPaleRed: 'hsl(12, 100%, 96%)',
-        veryLightGray: 'hsl(0,0%,98%)'
-        
+      borderRadius:{
+
+        extraLarge:'16rem'
       },
     },
   },
